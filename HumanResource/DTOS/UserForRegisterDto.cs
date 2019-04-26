cc9e6bf14e8034e5addda5e4a4ac1c3cdbb8ca05
ajay.thakur <ajay.thakur@IRIS-CSG-1333>
2019-04-26 11:38:45 +0530

    JWT for login

---
+++ HumanResource/DTOS/UserForRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanResource.DTOS
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 8 to 4 character.")]
+        public string password { get; set; }
+    }
+}
